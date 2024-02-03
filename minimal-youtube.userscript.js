7d66d8f508e6b404b18a00d6303c301c68eeffab
ashish <ashishaapan@gmail.com>
2024-02-03 17:53:03 +0530

    auto-commit

--- minimal-youtube.userscript.js
+++ minimal-youtube.userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      -
 // @description 21/01/2024, 15:53:30
 // @run-at       document-start
@@ -12,6 +12,6 @@
 
 
 // test setup working properly
-console.log("minimal youtube script live updated");
+console.log("minimal youtube script live updated again");
 
 

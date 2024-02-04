3bedc24f7f8be88ead003263d203c41d6d845964
ashish <ashishaapan@gmail.com>
2024-02-04 18:26:14 +0530

    auto-commit

--- minimal-youtube.userscript.js
+++ minimal-youtube.userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.2
+// @version     1.0
 // @author      -
 // @description 21/01/2024, 15:53:30
 // @run-at       document-start
@@ -11,7 +11,55 @@
 
 
 
-// test setup working properly
-console.log("minimal youtube script live updated again");
+const visible_path = ['subscriptions', 'library', 'history', 'videos', 'playlist', 'results', 'channel', '/c/', 'user'];
+
+function is_visible(path){
+
+
+  for(let i = 0;i<visible_path.length;++i){
+
+    if (path.search(visible_path[i])!==-1){
+      return true;
+
+    }
+
+  }
+  return false;
+
+}
+
+function hide_elements(selector){
+    //console.log("hide selector called with "+selector);
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((el)=>{el.style.cssText+='display: none!important;'});
+
+    return elements.length;
+
+}
+
+function inspector(){
+  // console.log('inspector here');
+
+  const curr_path = window.location.pathname;
+  if(!is_visible(curr_path)){
+
+    hide_elements('#video-title');
+    hide_elements('ytd-thumbnail');
+    hide_elements('ytd-video-meta-block');
+    
+    // testing different selectors
+    // hide_elements('ytd-video-meta-block');
+    // hide_elements('#contents.style-scope.ytd-rich-grid-renderer');
+    // hide_elements('yt-formatted-string#video-title');
+
+  }
+
+
+
+
+
+
+}
+const interval = setInterval(inspector , 300);
 
 

498816f101c568a23e2a450c34b5de85b90ad028
ashish <ashishaapan@gmail.com>
2024-02-26 09:25:59 +0530

    fix: replaced 36 with paramter base in random generation

--- obsidian-canvas-gen.js
+++ obsidian-canvas-gen.js
@@ -2,14 +2,14 @@
 // sl - https://forum.obsidian.md/t/how-is-the-canvas-node-element-id-determined/50739
 
 
-// js or with 0 to truncate to an integer trick 
+// js bitwise OR with 0 to truncate to an integer trick 
 // sl -  https://stackoverflow.com/questions/48265771/js-bitwise-what-are-the-rules-of-math-random-2-being-interpreted-as-0-and 
 function generate_unique_string(len = 16,base = 36){
     let result = '';
     for(let i = 0;i<len;++i){
         // NOTE : string concatenation generates garbage 
         // TODO: see if you can eliminate this 
-        result += (Math.random()*36 | 0).toString(base);
+        result += (Math.random()*base | 0).toString(base);
     }
     return result;
 }
